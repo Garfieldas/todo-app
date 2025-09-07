bf1b82ceb392a1525ac17d235e5327d41268a851
Garieldas <dainiusrainys99@gmail.com>
2025-09-07 22:21:33 +0300

    Base setup
    - Deleted the counter.ts file and its related logic from main.ts.
    - Removed css rules from  style.css file, which contained default styles for the application.
    - Removed the TypeScript logo SVG file as it is no longer needed.
    - Added Tailwind css cdn for a project.
    - Updated main.ts to create a simple header to test if tailwind works.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const app = document.querySelector('#app');
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const h1 = document.createElement('h1');
+h1.textContent = 'Setup'
+h1.className = 'text-3xl';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+app?.appendChild(h1);
