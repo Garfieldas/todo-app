8ea396dbf12292c5478e290e768b9edaa5afe4c8
Garieldas <dainiusrainys99@gmail.com>
2025-09-10 22:50:46 +0300

    Added mobile menu and menu toggling on header button

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+const mobileMenu = document.querySelector('#mobileMenu');
+const navBtn = document.querySelector('#navBtn');
+
+navBtn?.addEventListener('click', () => mobileMenu?.classList.toggle('hidden'))
